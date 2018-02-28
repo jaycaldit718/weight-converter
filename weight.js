fdac4ed4b36e867d06dbb17e4cd532cae535d30c
Jason <36929319+jaycaldit718@users.noreply.github.com>
2018-02-28 15:18:13 -0500

    Update weight.js

--- weight.js
+++ weight.js
@@ -3,14 +3,15 @@ var input = document.querySelector("#input");
 var gramsDisplay = document.querySelector("#grams");
 var kilogramsDisplay = document.querySelector("#kilogramResult");
 
-var ounces;
+
 input.addEventListener("change", function () {
     var weight = input.value;
     var grams = weight * 453.592;
     document.querySelector("#gramResult").textContent = grams;
     var kilograms = weight * 0.45359237;
     document.querySelector("#kilogramResult").textContent = kilograms;
-
+    var ounces = weight * 16;
+    document.querySelector("#ounceResult").textContent = ounces;
 
 
 })
